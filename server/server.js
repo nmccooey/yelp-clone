4e449c72a66e9f4e152415328c02f7291c4d8688
Nicholas McCooey <nicholasmccooey@gmail.com>
2020-10-18 10:40:37 -0400

    Update a restaurant route.

--- server/server.js
+++ server/server.js
@@ -49,7 +49,7 @@ app.get("/api/v1/restaurants/:id", async (req, res) => {
 app.post("/api/v1/restaurants/", async (req, res) => {
   try {
     const results = await db.query(
-      "INSERT INTO restaurants(name, location, price_range) VALUES ($1, $2, $3) returning *",
+      "INSERT INTO restaurants (name, location, price_range) values ($1, $2, $3) returning *",
       [req.body.name, req.body.location, req.body.price_range]
     );
     res.status(201).json({
@@ -58,12 +58,24 @@ app.post("/api/v1/restaurants/", async (req, res) => {
         restaurant: results.rows[0],
       },
     });
-  } catch (error) {}
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-// Update a restaurant
+// UPDATE a restaurant
 app.put("/api/v1/restaurants/:id", (req, res) => {
-  console.log(req.params.id);
+  const results = await db.query(
+    "UPDATE restaurants SET name = $1, location = $2, price_range = $3 where id = $4 returning *",
+    [req.body.name, req.body.location, req.body.price_range, req.params.id]
+  );
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      restaurant: results.rows[0],
+    },
+  });
 });
 
 // DELETE a restaurant
