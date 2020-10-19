9a0f35c06cc9d55e9229ede567b0e67f0811f119
Nicholas McCooey <nicholasmccooey@gmail.com>
2020-10-19 11:39:36 -0400

    Added restaurant context

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,19 @@ import Home from "./routes/Home";
 import Update from "./routes/Update";
 import Detail from "./routes/Detail";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { RestaurantsContextProvider } from "./context/RestaurantsContext";
 
 const App = () => {
   return (
-    <Router>
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/restaurants/:id/update' component={Update} />
-        <Route exact path='/restaurants/:id' component={Detail} />
-      </Switch>
-    </Router>
+    <RestaurantsContextProvider>
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/restaurants/:id/update' component={Update} />
+          <Route exact path='/restaurants/:id' component={Detail} />
+        </Switch>
+      </Router>
+    </RestaurantsContextProvider>
   );
 };
 
